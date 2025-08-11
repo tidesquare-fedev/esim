a0333f4ab2c72e9a79e77a3fd4ef1d9fc940541a
unknown <lkil@tidesquare.com>
2025-08-11 10:54:52 +0900

    패키지 변경

---
+++ .next/server/chunks/709.js
@@ -0,0 +1,59 @@
+exports.id=709,exports.ids=[709],exports.modules={4240:(e,t,s)=>{Promise.resolve().then(s.bind(s,2993)),Promise.resolve().then(s.bind(s,958)),Promise.resolve().then(s.bind(s,7797)),Promise.resolve().then(s.bind(s,787)),Promise.resolve().then(s.t.bind(s,4064,23))},528:(e,t,s)=>{Promise.resolve().then(s.t.bind(s,2994,23)),Promise.resolve().then(s.t.bind(s,6114,23)),Promise.resolve().then(s.t.bind(s,9727,23)),Promise.resolve().then(s.t.bind(s,9671,23)),Promise.resolve().then(s.t.bind(s,1868,23)),Promise.resolve().then(s.t.bind(s,4759,23))},2993:(e,t,s)=>{"use strict";s.d(t,{default:()=>n});var r=s(326),i=s(7577),a=s(4563);function n({env:e="production"}){let[t,s]=(0,i.useState)(!1),[n,o]=(0,i.useState)(!1);return t?(0,r.jsxs)("div",{className:"z-50",children:[(0,r.jsxs)("div",{className:"h-[50px] bg-white flex items-center justify-center",children:[r.jsx("div",{className:"absolute left-[20px] top-[0px]",children:r.jsx("button",{className:"relative before:w-[28px] before:h-[10px] before:bg-[url('https://static.priviatravel.com/images/front/mtravel/contents/icon-common-secM.png')] before:bg-[length:160px] before:bg-[position:-45px_0] before:bg-[content:''] before:absolute before:left-[0] before:top-[6px]",onClick:()=>o(!n),children:r.jsx("span",{className:"absolute w-[1px] h-[1px] hidden",children:"메뉴보기"})})}),r.jsx("a",{href:"https://tmw.priviatravel.com",className:"w-[95px]",children:r.jsx("img",{src:"https://static.priviatravel.com/images/front/mtravel/contents/logo-privia-2.png",alt:"Privia"})})]}),n&&r.jsx(a.Z,{tagName:"mo-global-nav-bar",attributes:{env:e,visible:"Y",dim:"Y"},fallback:r.jsx("div",{className:"h-16 bg-white border-t border-gray-200 flex items-center justify-center",children:r.jsx("div",{className:"text-sm text-gray-500",children:"Loading..."})})})]}):null}},958:(e,t,s)=>{"use strict";s.d(t,{default:()=>n});var r=s(326),i=s(7577),a=s(4563);function n({env:e="production"}){let[t,s]=(0,i.useState)(!1);return t?r.jsx(a.Z,{tagName:"pc-global-nav-bar",attributes:{env:e,category:"TNA",nonesticky:"Y"},fallback:r.jsx("div",{className:"h-16 bg-white border-b border-gray-200 flex items-center justify-center",children:r.jsx("div",{className:"text-sm text-gray-500",children:"Loading..."})})}):null}},7797:(e,t,s)=>{"use strict";s.d(t,{default:()=>n});var r=s(326),i=s(7577),a=s(4563);function n({env:e="production"}){let[t,s]=(0,i.useState)(!1);return t?r.jsx("div",{className:"fixed bottom-0 left-0 right-0 z-[101]",children:r.jsx(a.Z,{tagName:"bottom-tab-bar-widget",attributes:{env:e},fallback:r.jsx("div",{className:"h-16 bg-white border-t border-gray-200 flex items-center justify-center",children:r.jsx("div",{className:"text-sm text-gray-500",children:"Loading..."})})})}):null}},787:(e,t,s)=>{"use strict";s.d(t,{default:()=>n});var r=s(326),i=s(7577),a=s(4563);function n({env:e="production"}){let[t,s]=(0,i.useState)(!1);return t?r.jsx(a.Z,{tagName:"gnb-widget",attributes:{env:e},fallback:r.jsx("div",{className:"h-16 bg-white border-b border-gray-200 flex items-center justify-center",children:r.jsx("div",{className:"text-sm text-gray-500",children:"Loading..."})})}):null}},4563:(e,t,s)=>{"use strict";s.d(t,{Z:()=>a});var r=s(326),i=s(7577);function a({tagName:e,attributes:t={},children:s,fallback:a=null}){let[n,o]=(0,i.useState)(!1),[d,l]=(0,i.useState)(!1);return n&&(d||!a)?r.jsx(e,{...t,children:s}):r.jsx(r.Fragment,{children:a})}},9514:(e,t,s)=>{"use strict";s.r(t),s.d(t,{default:()=>N,metadata:()=>y});var r=s(9510);s(7272);var i=s(9720),a=s(8570);let n=(0,a.createProxy)(String.raw`C:\Users\ts1042\esim-3\components\tourvis-pc-gnb.tsx`),{__esModule:o,$$typeof:d}=n;n.default;let l=(0,a.createProxy)(String.raw`C:\Users\ts1042\esim-3\components\tourvis-pc-gnb.tsx#default`),c=(0,a.createProxy)(String.raw`C:\Users\ts1042\esim-3\components\tourvis-bottom-tab-bar.tsx`),{__esModule:m,$$typeof:p}=c;c.default;let g=(0,a.createProxy)(String.raw`C:\Users\ts1042\esim-3\components\tourvis-bottom-tab-bar.tsx#default`),v=(0,a.createProxy)(String.raw`C:\Users\ts1042\esim-3\components\privia-pc-gnb.tsx`),{__esModule:u,$$typeof:h}=v;v.default;let f=(0,a.createProxy)(String.raw`C:\Users\ts1042\esim-3\components\privia-pc-gnb.tsx#default`),x=(0,a.createProxy)(String.raw`C:\Users\ts1042\esim-3\components\privia-hamburger.tsx`),{__esModule:b,$$typeof:w}=x;x.default;let j=(0,a.createProxy)(String.raw`C:\Users\ts1042\esim-3\components\privia-hamburger.tsx#default`),y={title:"eSIM 예약 서비스",description:"eSIM 예약 서비스 프로토타입"};async function N({children:e}){return(0,r.jsxs)("html",{lang:"en",children:[r.jsx("head",{children:r.jsx("link",{rel:"stylesheet",href:"https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"})}),(0,r.jsxs)("body",{children:["tourvis"===process.env.NEXT_PUBLIC_APP_BRAND&&(0,r.jsxs)(r.Fragment,{children:[r.jsx(i.default,{src:`https://${"development"===process.env.NEXT_PUBLIC_APP_ENV?"t":""}account.tourvis.com/api/v1/lgn/sc`,strategy:"afterInteractive"}),r.jsx(i.default,{id:"tourvis-script",src:`https://${"development"===process.env.NEXT_PUBLIC_APP_ENV?"d1ad4gpy4661in":"d2um1hurm6o2hd"}.cloudfront.net/tourvis-static/activity/scripts/tourvis_script.js`,strategy:"afterInteractive"}),r.jsx(i.default,{src:`https://${"production"===process.env.NEXT_PUBLIC_APP_ENV?"d2um1hurm6o2hd":"d1ad4gpy4661in"}.cloudfront.net/tourvis-static/common/common-widget.js?20250325`,strategy:"afterInteractive"}),r.jsx(l,{env:"production"===process.env.NEXT_PUBLIC_APP_ENV?"production":"development"})]}),"privia"===process.env.NEXT_PUBLIC_APP_BRAND&&(0,r.jsxs)(r.Fragment,{children:[r.jsx(i.default,{src:`https://${"production"===process.env.NEXT_PUBLIC_APP_ENV?"auth":"tauth"}.priviatravel.com/check`,strategy:"afterInteractive"}),r.jsx(i.default,{src:`https://${"production"===process.env.NEXT_PUBLIC_APP_ENV?"www":"twww"}.priviatravel.com/common/sso/sc`,strategy:"afterInteractive"}),r.jsx(i.default,{id:"sc1",type:"text/javascript",children:`
+              var getCookie = function (name) {
+                var dc = document.cookie;
+                var prefix = name + "=";
+                var begin = dc.indexOf("; " + prefix);
+
+                if (begin == -1) {
+                    begin = dc.indexOf(prefix);
+                    if (begin != 0) { return null;}
+                } else {
+                    begin += 2;
+                }
+
+                var end = document.cookie.indexOf(";", begin);
+
+                if (end == -1) {
+                    end = dc.length;
+                }
+
+                return unescape(dc.substring(begin + prefix.length, end));
+              }
+
+              var gtmMemberId = getCookie("memberId");
+              var gtmMemberNo = getCookie("gaUniqDemension1");
+              var filter = "win16|win32|win64|mac|macintel";
+              var agent = navigator.userAgent.toLowerCase();
+              var platform = navigator.platform.toLowerCase();
+              var appName = '';
+              var device = "";
+
+              if (agent.indexOf('privia_travel_android_app_ver') > -1) {
+                appName = 'mobileApp';
+                device = "moApp";
+              } else if (agent.indexOf('privia_travel_ios_app_ver') > -1) {
+                appName = 'mobileApp';
+                device = "moApp";
+              } else if (agent.indexOf('android') > -1) {
+                appName = 'mobileWeb';
+                device = "moWeb";
+              } else if (agent.indexOf('iphone') > -1 || agent.indexOf('ipad') > -1 || agent.indexOf('ipod') > -1) {
+                appName = 'mobileWeb';
+                device = "moWeb";
+              } else if (agent.indexOf('mobile') > -1) {
+                appName = 'mobileWeb';
+                device = "moWeb";
+              } else {
+                appName = 'desktop';
+                device = "pcWeb";
+              }
+
+              window.dataLayer = window.dataLayer || [];
+              dataLayer = [{
+                'userID' : gtmMemberId != null ? gtmMemberId : "",
+                'appName' : appName,
+                'dimension1' : gtmMemberNo != null ? atob(gtmMemberNo) : "",
+                'dimension10' : 'tna',
+                'dimension11' : device
+              }];
+              `}),r.jsx(i.default,{id:"sc2",type:"text/javascript",children:"(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WQKP64D');"}),r.jsx(i.default,{id:"sc3",type:"text/javascript",children:"(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5HT7Z7K');"}),r.jsx(i.default,{id:"sc4",type:"text/javascript",children:"(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M72MGBC');"}),r.jsx(i.default,{id:"sc5",type:"text/javascript",children:"(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WJ3CZHH');"}),r.jsx(i.default,{src:`https://${"production"===process.env.NEXT_PUBLIC_APP_ENV?"":"t"}www.priviatravel.com/widget/common-wc-widget.bundle.js`,strategy:"afterInteractive"}),r.jsx(f,{env:"production"===process.env.NEXT_PUBLIC_APP_ENV?"production":"development"}),r.jsx(j,{env:"production"===process.env.NEXT_PUBLIC_APP_ENV?"production":"development"})]}),e,"tourvis"===process.env.NEXT_PUBLIC_APP_BRAND&&(0,r.jsxs)(r.Fragment,{children:[r.jsx(g,{env:"production"===process.env.NEXT_PUBLIC_APP_ENV?"production":"development"}),r.jsx("noscript",{children:r.jsx("iframe",{src:"https://www.googletagmanager.com/ns.html?id=GTM-P8NH3S5",height:"0",width:"0",style:{display:"none",visibility:"hidden"}})}),r.jsx("noscript",{children:r.jsx("iframe",{src:"https://www.googletagmanager.com/ns.html?id=GTM-5J7Q7XV",height:"0",width:"0",style:{display:"none",visibility:"hidden"}})})]}),"privia"===process.env.NEXT_PUBLIC_APP_BRAND&&(0,r.jsxs)(r.Fragment,{children:[r.jsx("noscript",{id:"nsc1",dangerouslySetInnerHTML:{__html:'<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQKP64D" height="0" width="0" style="display:none;visibility:hidden"></iframe>'}}),r.jsx("noscript",{id:"nsc2",dangerouslySetInnerHTML:{__html:'<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5HT7Z7K" height="0" width="0" style="display:none;visibility:hidden"></iframe>'}}),r.jsx("noscript",{id:"nsc3",dangerouslySetInnerHTML:{__html:'<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M72MGBC" height="0" width="0" style="display:none;visibility:hidden"></iframe>'}}),r.jsx("noscript",{id:"nsc4",dangerouslySetInnerHTML:{__html:'<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJ3CZHH" height="0" width="0" style="display:none;visibility:hidden"></iframe>'}})]})]})]})}},7272:()=>{}};
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"Z/keoeUp+VfhVhNbDID6xbB4Qd91wgpKe6NfGnLHPNM=\"}"
